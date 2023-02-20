6a6d11052aa04d63f53a8e6aa29eae41e915da20
Sangeeth <116724382+sangeethvelu@users.noreply.github.com>
2023-02-20 14:27:19 +0530

    Update About and Navbar
    Updated img alt attributes and button aria description

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import styles from "./About.module.css";
 
 export default function About() {
-    return (
-        <section className={styles["about-section"]} id="about">
-            <h2 className={styles["about-header"]}>About</h2>
-            <div className={styles["about-content"]}>
-                <img src="/assets/img/3d-boy.svg" />
-                <p className={styles["about-description"]}>"A highly motivated and results-driven Front-End Developer striving to create and implement user-friendly, visually appealing responsive websites and web applications. Proficient in HTML, CSS, JavaScript, and frameworks such as React. Strong ability to turn designs into code and collaborate with cross-functional teams to deliver high-quality projects on time. Adept in optimizing website performance, ensuring cross-browser compatibility, and ensuring accessibility standards are met"</p>
-            </div>
-        </section>
-    )
+  return (
+    <section className={styles["about-section"]} id="about">
+      <h2 className={styles["about-header"]}>About</h2>
+      <div className={styles["about-content"]}>
+        <img src="/assets/img/3d-boy.svg" alt="Image of a boy" />
+        <p className={styles["about-description"]}>
+          "A highly motivated and results-driven Front-End Developer striving to
+          create and implement user-friendly, visually appealing responsive
+          websites and web applications. Proficient in HTML, CSS, JavaScript,
+          and frameworks such as React. Strong ability to turn designs into code
+          and collaborate with cross-functional teams to deliver high-quality
+          projects on time. Adept in optimizing website performance, ensuring
+          cross-browser compatibility, and ensuring accessibility standards are
+          met"
+        </p>
+      </div>
+    </section>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,31 +3,41 @@ import styles from "./Navbar.module.css";
 import { HashLink } from "react-router-hash-link";
 import resume from "/assets/documents/resume.pdf";
 
-
 function Navbar() {
+  const [hide, setHide] = React.useState(true);
 
-    const [hide, setHide] = React.useState(true);
+  function showMobileNav() {
+    setHide((prevState) => !prevState);
+  }
 
-    function showMobileNav() {
-        setHide(prevState => !prevState);
-    }
-
-    return (
-        <>
-            <button className={`${styles["mobile-nav-toggle"]} ${styles[hide ? "" : "show-img"]}`} onClick={showMobileNav}></button>
-            <nav className={`${styles["nav-section"]}`}>
-                <div className={styles["nav-details"]}>
-                    <img src="/assets/img/sangeeth.jpg" className={styles["nav-img"]} alt="Image of Sangeeth"/>
-                    <p className={styles["nav-name"]}>Sangeeth Velusamy</p>
-                </div>
-                <div className={`${styles["nav-info"]} ${styles[hide ? "hide" : ""]}`}>
-                    <a href={resume} target="_blank">Resume</a>
-                    <HashLink to="/#about">About</HashLink>
-                    <HashLink to="/#contact">Contact</HashLink>
-                </div>
-            </nav>
-        </>
-    )
+  return (
+    <>
+      <button
+        className={`${styles["mobile-nav-toggle"]} ${
+          styles[hide ? "" : "show-img"]
+        }`}
+        aria-label="Navigation Bar"
+        onClick={showMobileNav}
+      ></button>
+      <nav className={`${styles["nav-section"]}`}>
+        <div className={styles["nav-details"]}>
+          <img
+            src="/assets/img/sangeeth.jpg"
+            className={styles["nav-img"]}
+            alt="Image of Sangeeth"
+          />
+          <p className={styles["nav-name"]}>Sangeeth Velusamy</p>
+        </div>
+        <div className={`${styles["nav-info"]} ${styles[hide ? "hide" : ""]}`}>
+          <a href={resume} target="_blank">
+            Resume
+          </a>
+          <HashLink to="/#about">About</HashLink>
+          <HashLink to="/#contact">Contact</HashLink>
+        </div>
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
