17973d6d32c660974dd648e0f9ad65ab3a3a000a
Sangeeth <116724382+sangeethvelu@users.noreply.github.com>
2023-03-01 19:16:25 +0530

    Update Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,10 +11,9 @@ export default function Experience() {
                 <p>September 2020 - September 2022</p>
                 <p className={styles["experience-location"]}>Chennai, Tamilnadu</p>
                 <ul>
-                    <li>- TBD</li>
-                    <li>- TBD</li>
-                    <li>- TBD</li>
-                    <li>- TBD</li>
+                    <li>- Created and managed the documentation page for client website using HTML, CSS and JavaScript.</li>
+                    <li>- Optimized website performance by implementing best practices for loading times, and browser compatibility.</li>
+                    <li>- Worked with version control systems such as Git and GitHub to manage code versions, facilitate collaboration, and streamline development processes.</li>
                 </ul>
             </div>
         </section>
